c3578528cf163cdb3909b542b27fece791f2ffc4
UltimateAbhinav <72779243+UltimateAbhinav@users.noreply.github.com>
2024-07-13 15:24:41 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Abhinav | Personal Portfolio",
+  description: "Abhinav is a full-stack developer and a final year undergrad",
 };
 
 export default function RootLayout({
